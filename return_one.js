529dff318e1df20ca16a6c484e1f62023b2d29d5
csyisgod123 <129401628+csyisgod123@users.noreply.github.com>
2023-03-30 23:39:56 +0800

    Add files via upload

---
+++ return_one.js
@@ -0,0 +1,6 @@
+﻿
+function view_the_one(IDnum) {//函数名与onclink出调用名字一样即可
+    var IDnum = document.getElementById("IDnum").value;
+    var  url = 'http://localhost:8042/patients/' + IDnum;//是就向url所指发送信息
+    return url;
+}
---
+++ return_view.js
@@ -0,0 +1,26 @@
+﻿
+    //获取输入的ID的详细信息
+
+function view_the_load() {//函数名与onclink出调用名字一样即可
+
+    var url = 'http://localhost:8042/patients'//是就向url所指发送信息
+    return url;
+}
+   /*f (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();//  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+            }
+    else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");// IE6, IE5 浏览器执行代码
+            }//不同浏览器执行代码
+   /*mlhttp.onreadystatechange = function () {//定义xmlhttp下的“onreadystatechange”
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {//状态码确认
+        responsecontent = xmlhttp.responseText;
+
+    Res =responsecontent;
+    document.getElementById("result").innerText = Res;
+                }
+            }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+        }//显示已上传患者ID
+        */
